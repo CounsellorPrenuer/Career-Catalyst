31fe561a3587087f955280dc09683138d60da377
Gaurav <your-email@gmail.com>
2026-02-19 22:01:45 +0530

    deploy: finalize with useCdn false and correct pathing

--- client/src/sanity/client.ts
+++ client/src/sanity/client.ts
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client';
 export const client = createClient({
     projectId: 'ua6tzmti',
     dataset: 'production',
-    useCdn: true, // set to `false` to bypass the edge cache
+    useCdn: false, // set to `false` to bypass the edge cache
     apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
 });
